41cfa182e0346230f2c94cd1c1e814b8fb270380
Lars Hanisch <dvb@flensrocker.de>
2025-01-22 08:55:01 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterOutlet],
+  template: `<h1>Smart-View-Injection</h1>
+    <router-outlet />`,
+})
+export class AppComponent {}
